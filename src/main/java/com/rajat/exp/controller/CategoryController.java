628ecc81ce9c103845700b1fc4a9299c7492c80c
rajatmandewala <rajatmandewala@gmail.com>
2020-04-04 20:28:41 +0530

    crossorigin anotaion added

--- src/main/java/com/rajat/exp/controller/CategoryController.java
+++ src/main/java/com/rajat/exp/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.rajat.exp.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.rajat.exp.service.CategoryService;
 
 @RestController
 @RequestMapping("/category")
+@CrossOrigin
 public class CategoryController {	
 	
 	@Autowired
